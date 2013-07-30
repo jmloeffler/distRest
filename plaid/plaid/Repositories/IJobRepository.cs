c5908808a6ec2074fc42c8a18ccb6ef3a412e0f6
jmloeffler <jmloeffler@gmail.com>
2013-07-29 23:33:53 -0500

    Adding the WebAPI project. Tinkering with a repository to hold the jobs.

---
+++ plaid/plaid/Repositories/IJobRepository.cs
@@ -0,0 +1,21 @@
+﻿using plaid.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace plaid.Controllers
+{
+    public interface IJobRepository
+    {
+        IEnumerable<Job> GetJobs();
+    }
+
+    public class JobRepository : IJobRepository
+    {
+        public IEnumerable<Job> GetJobs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
